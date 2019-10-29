9bd5c0e5991d8fbc107555bf4804a63a30e7e2ab
Kamil Jacko <33236011+limak360@users.noreply.github.com>
2019-10-29 23:35:45 +0100

    Add files via upload

---
+++ ChatMessage.java
@@ -0,0 +1,21 @@
+package com.kamilj.demo;
+
+public class ChatMessage {
+
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public ChatMessage() {
+    }
+
+    public ChatMessage(String value) {
+        this.value = value;
+    }
+}
